ebd3bc963a971ccae8e8d295baf7ae6c44a7e4d2
chethanyadav456 <chethanyadav456@gmail.com>
2025-02-27 17:05:38 +0530

    first commit

---
+++ src/Syntax.java
@@ -0,0 +1,10 @@
+public class Syntax {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+
+        // Iterating from 1 to 5
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
